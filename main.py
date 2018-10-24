8542c2d0ad5f947716df23436bd84d2670a9357d
Sean Frischmann <stfrischmann@gmail.com>
2018-10-24 17:41:35 -0400

    added ability to customize where tasks go from command line

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ with open(os.path.join(sys.path[0], 'config.json')) as configFile:
 
 
 def main():
-    for field in ['USER', 'PASSWORD', 'JIRA', 'JQL', 'TODOIST', 'ISSUELINK']:
+    fields = ['USER', 'PASSWORD', 'JIRA', 'JQL', 'TODOIST', 'ISSUELINK', 'PROJECT_OPTIONS']
+    for field in fields:
         if (field not in config):
             print "Config File is missing:", field
             exit()
@@ -22,9 +23,23 @@ def main():
     activeTasks = makeRequest({"Authorization": "Bearer %s" % config['TODOIST']},
             {}, {"url": "https://beta.todoist.com/API/v8/tasks"})
 
+    projects = makeRequest({"Authorization": "Bearer %s" % config['TODOIST']},
+            {}, {"url": "https://beta.todoist.com/API/v8/projects"})
+
+    labels = makeRequest({"Authorization": "Bearer %s" % config['TODOIST']},
+            {}, {"url": "https://beta.todoist.com/API/v8/labels"})
+
     taskList = {}
     for task in activeTasks:
         taskList[task['content']] = task
+
+    projectList = {}
+    for project in projects:
+        projectList[project['name']] = project
+
+    labelList = {}
+    for label in labels:
+        labelList[label['name']] = label
 
     headers = {
         'Content-Type': 'application/json',
@@ -42,13 +57,37 @@ def main():
     if (data):
         todoapi = todoist.TodoistAPI(config['TODOIST'])
 
+        if ('Jira-Issue' not in labelList):
+            label = todoapi.labels.add('Jira-Issue')
+            todoapi.commit()
+            labelList['Jira-Issue'] = label
+
+        configureInInbox = raw_input("Send (" + str(data['total']) +
+                ") tasks to Inbox? (yes|no)\n")
+
         for issue in data['issues']:
             title = '[**' + issue['key'] + '**](' + config['ISSUELINK'] + issue['key']
             title +=  ') ' + issue['fields']['summary']
 
             if (title not in taskList):
                 print 'adding task: ', title
-                item = todoapi.items.add(title, 0)
+
+                projectID = 0
+                dueDate = ''
+
+                if (configureInInbox == 'no'):
+                    if ('PROJECT_OPTIONS' in config):
+                        project = raw_input("Choose project: " +
+                                config['PROJECT_OPTIONS'] + "\n")
+
+                        if (project in projectList):
+                            projectID = projectList[project]['id']
+
+                    dueDate = raw_input("Choose a due date (ex. every day)\n")
+
+                item = todoapi.items.add(content=title, project_id=projectID,
+                        labels=[labelList['Jira-Issue']['id']], date_string=dueDate)
+
                 todoapi.commit()
 
                 note = todoapi.notes.add(item['id'], issue['fields']['description'])
